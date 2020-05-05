e12ee407044767cec9054562cbc7eeb52fa314cf
Aniyogakiza <aimable.niyogakiza@drpgroup.com>
2020-05-05 10:40:00 +0200

    Billards setup

---
+++ src/utils/constants.js
@@ -0,0 +1,15 @@
+const pointLightPositions = [
+  [-5, -12, 20],
+  [5, -12, 20],
+  [-5, 12, 20],
+  [5, 12, 20]
+]
+
+const tableDimensions = {
+  topLeft: { x: -12, y: 24},
+  topRight: { x: 12, y: 24},
+  bottomLeft: { x: -12, y: -24},
+  bottomRight: { x: 12, y: -24}
+}
+
+export default { pointLightPositions, tableDimensions }
