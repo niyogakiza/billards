60b97e225fda83dfb67a7d39e5a7b201e7e67fcb
niyogakiza <niyoai@yahoo.fr>
2020-05-07 19:12:16 +0000

    Deploying to gh-pages from master @ e5aa5ab669e14bd0897f8cb2b8c06a746d63b9b4 🚀

---
+++ precache-manifest.e7079adab8d067e1d51cd77d9f623770.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a43eaf6df280fdd1df3467a8d5867a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a46b03e57628161c5e44",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b7c0c85997d57ef03050",
+    "url": "/static/js/2.60acb0bf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.60acb0bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a46b03e57628161c5e44",
+    "url": "/static/js/main.dbfef29c.chunk.js"
+  },
+  {
+    "revision": "33d446b4134b68a24f91",
+    "url": "/static/js/runtime-main.441aa74c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
